7b2b1bc7f9007bac81a588b9b795bd2af7102c20
Ilya Kaznacheev <ilya@kaznacheev.me>
2019-05-31 19:52:48 +0300

    1.1 Algorithms

---
+++ chapter-1/section-1-1.go
@@ -0,0 +1,19 @@
+package chapter
+
+// Section 1.1 Algorithms
+
+// InsertionSort takes as a parameter an array A[1..n] containing a sequence of length n that is to be sorted. (In the code, the number n of elements in A is denoted by length[A].) The input numbers are sorted in place: the numbers are rearranged within the array A, with at most a constant number of them stored outside the array at any time. The input array A contains the sorted output sequence when InsertionSort is finished.
+func InsertionSort(a []int) []int {
+	var i, j int
+	for j = 1; j < len(a); j++ {
+		key := a[j]
+		// Insert A[j] into the sorted sequence A[1 . . j - 1].
+		i = j - 1
+		for i >= 0 && a[i] > key {
+			a[i+1] = a[i]
+			i--
+		}
+		a[i+1] = key
+	}
+	return a
+}
---
+++ chapter-1/section-1-1_test.go
@@ -0,0 +1,51 @@
+package chapter
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInsertionSort(t *testing.T) {
+	tests := []struct {
+		name string
+		a    []int
+		b    []int
+	}{
+		{
+			name: "one",
+			a:    []int{1},
+			b:    []int{1},
+		},
+
+		{
+			name: "two",
+			a:    []int{2, 1},
+			b:    []int{1, 2},
+		},
+
+		{
+			name: "same",
+			a:    []int{2, 2, 2},
+			b:    []int{2, 2, 2},
+		},
+
+		{
+			name: "many",
+			a:    []int{5, 2, 3, 3, 1},
+			b:    []int{1, 2, 3, 3, 5},
+		},
+
+		{
+			name: "exercise 1.1",
+			a:    []int{31, 41, 59, 26, 41, 58},
+			b:    []int{26, 31, 41, 41, 58, 59},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := InsertionSort(tt.a); !reflect.DeepEqual(got, tt.b) {
+				t.Errorf("InsertionSort() = %v, want %v", got, tt.b)
+			}
+		})
+	}
+}
